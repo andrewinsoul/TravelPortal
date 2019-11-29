d9428db0d6b440667b7190098e52ff2a1a2c2ae6
Andrew Okoye <andrewinsoul@gmail.com>
2019-11-29 12:54:43 +0100

    seed database

--- travelportal_api/apps/TravelPortalSearch/models.py
+++ travelportal_api/apps/TravelPortalSearch/models.py
@@ -3,33 +3,33 @@ from enum import Enum
 from django.core.exceptions import ValidationError
 from django.utils.translation import gettext_lazy as _
 
-class Cabin_Class(Enum):
-  F = 'First Class'
-  E = 'Economy Class'
-  P = 'Premium Class'
-  B = 'Business Class'
-  A = 'All Class'
-
-
-def validate_city_code(value:str) -> str:
+def validate_airport_code(value:str) -> str:
   if not value.isalpha():
     raise ValidationError(_('%(value) is not a valid code'), params={'value': value})
 
-class City(models.Model):
+class Airport(models.Model):
   name = models.CharField(max_length=90)
-  code = models.CharField(max_length=3, validators=[validate_city_code])
+  code = models.CharField(max_length=3, validators=[validate_airport_code])
   country = models.CharField(max_length=40)
 
   def __str__(self:object) -> tuple:
     return self.name, self.country
 
 class Flight(models.Model):
+  Cabin_Class = (
+    ('First', 'First Class'),
+    ('Business', 'Business Class'),
+    ('Premium', 'Premium Class'),
+    ('Economy', 'Economy Class'),
+    ('All', 'All Class')
+  )
   name = models.CharField(max_length=70)
-  city = models.ForeignKey(City, on_delete=models.CASCADE)
+  depature = models.ForeignKey(Airport, on_delete=models.CASCADE, related_name='depart_from_airport')
+  arrival = models.ForeignKey(Airport, on_delete=models.CASCADE, related_name='arrive_at_airport')
   price = models.IntegerField()
   departure_date = models.DateTimeField()
   return_date = models.DateTimeField()
-  cabin_class = models.CharField(max_length=4, choices=[(class_cat, class_cat.value) for class_cat in Cabin_Class])
+  cabin_class = models.CharField(max_length=54, choices=Cabin_Class, default='All')
 
   def __str__(self:object) -> tuple:
     return self.name, self.price
