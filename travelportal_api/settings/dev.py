7a975f03c12e0e7ed46c78d32cc67ff130af48f9
Andrew Okoye <andrewinsoul@gmail.com>
2019-11-29 05:46:19 +0100

    setup development environment

---
+++ travelportal_api/settings/dev.py
@@ -0,0 +1,43 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+ALLOWED_HOSTS = []
+
+
+# Application definition
+
+INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'travelportal_api.apps.TravelPortalSearch'
+]
+
+
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'PORT': os.getenv('DB_PORT'),
+        'HOST': os.getenv('DB_HOST'),
+        'TEST': {
+            'CHARSET': None,
+            'COLLATION': None,
+            'NAME': 'test.db',
+            'PASSWORD': os.getenv('DB_PASSWORD'),
+            'HOST': 'postgres',
+            'PORT': 5432,
+            'USER': 'postgres',
+            'MIRROR': None
+        }
+
+    }
+}
